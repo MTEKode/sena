813b845b10a42f1c5ed543bdee5d256d5bf17dac
Marcos Toribio <mtoribio@ayesa.com>
2025-06-17 18:54:20 +0200

    push forget files

---
+++ test/controllers/emoti_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EmotiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/emoti_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EmotiTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
