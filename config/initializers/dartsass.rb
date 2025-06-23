dd8ddf0cab16f28783ba21d09cd24ac49be62250
Marcos <marcostoribio10@gmail.com>
2025-06-23 10:16:19 +0200

    fixing assets poll only for dev

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,5 +1,5 @@
 # config/initializers/dartsass.rb
 Rails.application.config.dartsass.build_options << "--poll" if Rails.env.development?
 Rails.application.config.dartsass.builds = {
-  "*.sass"  => "*.css",
+  "application.sass"  => "application.css",
 }
